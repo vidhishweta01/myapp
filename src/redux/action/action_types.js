4b3d9e7db54fac4f317ba8e48b974125ca8cd674
vidhishweta01 <vidhishweta01@gmail.com>
2021-12-11 12:56:38 +0530

    template

--- src/redux/action/action_types.js
+++ src/redux/action/action_types.js
@@ -1,19 +1,5 @@
 const ActionTypes = {
-  SpaLoading: 'SpaLoading',
-  SpaSuccess: 'SpaSuccess',
-  SpaFailure: 'SpaFailure',
-  ServicesLoading: 'ServicesLoading',
-  ServicesSucess: 'ServicesSucess',
-  ServicesFailure: 'ServicesFailure',
-  WorkScheduleLoading: 'WorkScheduleLoading',
-  WorkScheduleSuccess: 'WorkScheduleSuccess',
-  WorkScheduleFailure: 'WorkScheduleFailure',
-  getTotalBoookingLoading: 'getTotalBoookingLoading',
-  getTotalBoookingSuccess: 'getTotalBookingSuccess',
-  getTotalBoookingFailure: 'getTotalBookingFailure',
-  CreateBookingLoading: 'CreateBookingLoading',
-  CreateBookingSuccess: 'CreateBookingSuccess',
-  CreateBookingFailure: 'CreateBookingFailure',
+ 
 };
 
 export default ActionTypes;
--- src/redux/reducers/root_reducer.js
+++ src/redux/reducers/root_reducer.js
@@ -1,12 +1,9 @@
 import { combineReducers } from 'redux';
-import GetSpaReducer from './spa_reducer';
 import GetServiceReducer from './service_reducer';
-import workScheduleReducer from './work_schedule_reducer';
+
 
 const rootReducer = combineReducers({
-  GetSpaReducer,
   GetServiceReducer,
-  workScheduleReducer,
 });
 
 export default rootReducer;
